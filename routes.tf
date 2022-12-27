8f5095ea06caafbb40cb610ee952395631c4d948
bjreddy009037 <bjreddy246@gmail.com>
2022-12-27 13:15:52 +0000

    terraform

--- routes.tf
+++ routes.tf
@@ -13,11 +13,11 @@ resource "aws_route_table_association" "assco" {
 }
 
 
-#resource "aws_route" "default-vpc-rt" {
-#  route_table_id = aws_route_table.route.id
-#  destination_cidr_block = data.terraform_remote_state.tgw.outputs.DEFAULT_VPC_CIDR
-#  transit_gateway_id = data.terraform_remote_state.tgw.outputs.TRANSIT_GW
-#}
+resource "aws_route" "default-vpc-rt" {
+  route_table_id = aws_route_table.route.id
+  destination_cidr_block = data.terraform_remote_state.tgw.outputs.DEFAULT_VPC_CIDR
+  transit_gateway_id = data.terraform_remote_state.tgw.outputs.TRANSIT_GW
+}
 
 resource "aws_route" "app-vpc-rt-in-default-vpc-rt" {
   route_table_id = data.terraform_remote_state.tgw.outputs.DEFAULT_VPC_RT

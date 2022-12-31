44d6290aeea1e97ffe845be7f87408f1a6272731
bjreddy009037 <bjreddy246@gmail.com>
2022-12-31 22:21:47 +0000

    terraform

--- tgw.tf
+++ tgw.tf
@@ -1,8 +1,23 @@
 resource "aws_ec2_transit_gateway_vpc_attachment" "component-attach" {
-  subnet_ids         = aws_subnet.main.*.id
-  transit_gateway_id = data.terraform_remote_state.tgw.outputs.TRANSIT_GW
-  vpc_id             = aws_vpc.main.id
-  tags = {
+  subnet_ids                                      = aws_subnet.main.*.id
+  transit_gateway_id                              = data.terraform_remote_state.tgw.outputs.TRANSIT_GW
+  transit_gateway_default_route_table_association = false
+  transit_gateway_default_route_table_propagation = false
+  vpc_id                                          = aws_vpc.main.id
+  tags                                            = {
     Name = "${var.COMPONENT}-tgw-attach-${var.ENV}"
   }
+}
+
+
+resource "aws_ec2_transit_gateway_route_table_association" "component-vpc-rt-ass" {
+  transit_gateway_attachment_id  = aws_ec2_transit_gateway_vpc_attachment.component-attach.id
+  transit_gateway_route_table_id = data.terraform_remote_state.tgw.outputs.APP_VPC_TRANSIT_GW_RT_TABLE
+}
+
+
+resource "aws_ec2_transit_gateway_route" "internet-rt-to-app-vpc" {
+  destination_cidr_block         = var.VPC_CIDR_BLOCK
+  transit_gateway_attachment_id  = aws_ec2_transit_gateway_vpc_attachment.component-attach.id
+  transit_gateway_route_table_id = data.terraform_remote_state.tgw.outputs.DEFAULT_VPC_TRANSIT_GW_ROUTE_TABLE
 }
